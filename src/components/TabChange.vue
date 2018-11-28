ff417fab64395b909f5b10d6347e8c4157a48b07
chengjiang <15712859926@163.com>
2018-11-28 08:26:13 +0800

    抽离tabChange组件

---
+++ src/components/TabChange.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="tab_change">
+    <b-container fluid>
+      <b-row>
+        <b-col
+          cols="6"
+          sm="6"
+          md="6"
+          lg="6"
+          xl="6"
+          class="text-center"
+        >
+          <img
+            src="../assets/images/income.png"
+            alt=""
+            class="img-fluid income mt-4"
+            @click="addActive1"
+          >
+          <p
+            class="text-center"
+            :class="{active:isActive1}"
+          >收入</p>
+        </b-col>
+        <b-col
+          cols="6"
+          sm="6"
+          md="6"
+          lg="6"
+          xl="6"
+          class="text-center"
+        >
+          <img
+            src="../assets/images/withdraw.png"
+            alt=""
+            class="img-fluid withdraw mt-4"
+            @click="addActive2"
+          >
+          <p
+            class="text-center"
+            :class="{active:isActive2}"
+          >提现</p>
+        </b-col>
+      </b-row>
+      <Tab class="tab1" />
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Tab from '../components/Tab';
+export default {
+  name: '',
+  data() {
+    return {
+      isActive1: true,
+      isActive2: false,
+    }
+  },
+  components: {
+    Tab
+  },
+  methods: {
+    addActive1() {
+      this.isActive1 = true;
+      this.isActive2 = false;
+    },
+    addActive2() {
+      this.isActive2 = true;
+      this.isActive1 = false;
+    }
+  }
+}
+</script>
+
+<style lang="" scoped>
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,87 +2,28 @@
   <div>
     <Header />
     <Carousel />
-    <div class="tab_change">
-      <b-container fluid>
-        <b-row>
-          <b-col
-            cols="6"
-            sm="6"
-            md="6"
-            lg="6"
-            xl="6"
-            class="text-center"
-          >
-            <img
-              src="../assets/images/income.png"
-              alt=""
-              class="img-fluid income mt-4"
-              @click="addActive1"
-            >
-            <p
-              class="text-center"
-              :class="{active:isActive1}"
-            >收入</p>
-          </b-col>
-          <b-col
-            cols="6"
-            sm="6"
-            md="6"
-            lg="6"
-            xl="6"
-            class="text-center"
-          >
-            <img
-              src="../assets/images/withdraw.png"
-              alt=""
-              class="img-fluid withdraw mt-4"
-              @click="addActive2"
-            >
-            <p
-              class="text-center"
-              :class="{active:isActive2}"
-            >提现</p>
-          </b-col>
-        </b-row>
-        <Tab class="tab1" />
-      </b-container>
-    </div>
+    <TabChange />
     <TabsChange />
   </div>
 </template>
 <script>
 import Header from '../components/Header';
 import Carousel from '../components/Carousel';
-import Tab from '../components/Tab';
 import TabsChange from '../components/Tabschange'
+import TabChange from '../components/TabChange'
 import '../assets/sass/index.sass';
 
 export default {
   data() {
     return {
-      isActive1: true,
-      isActive2: false,
+
     }
   },
   components: {
     Header,
     Carousel,
-    Tab,
-    TabsChange
-  },
-
-  created() {
-
-  },
-  methods: {
-    addActive1() {
-      this.isActive1 = true;
-      this.isActive2 = false;
-    },
-    addActive2() {
-      this.isActive2 = true;
-      this.isActive1 = false;
-    }
+    TabsChange,
+    TabChange
   }
 }
 </script>
