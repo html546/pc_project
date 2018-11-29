b728def766ee5e6b16d0862fef1d49f52b86ada9
chengjiang <15712859926@163.com>
2018-11-29 16:26:29 +0800

    注册页面修改

--- src/components/Register/RegisterPanel.vue
+++ src/components/Register/RegisterPanel.vue
@@ -22,84 +22,47 @@
             lg="7"
             xl="7"
           >
-            <div
-              class="mobile_panel mobile_panel1"
-            >
-              <b-form-group
-                v-for="(val,key) in reg_content"
-                :key="key"
-                :label="val.name"
-                label-for="register_input1"
-                label-text-align="left"
-                v-if="val.input!=='hidden'&&val.input=='text'"
-              >
-                <b-form-input
-                  id="register_input1"
-                  placeholder="请输入手机号"
-                  v-model="val.default"
-                  :name="val.name"
-                ></b-form-input>
-              </b-form-group>
-              <!-- <b-form-group
-                label="密码"
-                label-for="register_input2"
-                label-text-align="left"
-              >
-                <b-form-input
-                  id="register_input2"
-                  placeholder="*密码至少数字+密码,8-16位组成"
-                ></b-form-input>
-              </b-form-group>
-              <b-form-group
-                label="确认密码"
-                label-for="register_input3"
-                label-text-align="left"
-              >
-                <b-form-input
-                  id="register_input3"
-                  placeholder="*密码至少数字+密码,8-16位组成"
-                ></b-form-input>
-              </b-form-group>
-              <b-form-group
-                label="图片验证码"
-                label-for="register_input4"
-                label-text-align="left"
-              >
-                <b-form-input
-                  id="register_input4"
-                  placeholder="请输入验证码"
-                ></b-form-input>
-                <img
-                  src="../../assets/images/code.png"
-                  alt=""
+            <div class="mobile_panel mobile_panel1">
+              <b-form @submit="onSubmit" id="form">
+                <b-form-group
+                  v-for="(val,key) in reg_content"
+                  :key="key"
+                  :label="val.name"
+                  label-for="register_input1"
+                  label-text-align="left"
                 >
-                <div class="clearfix"></div>
-              </b-form-group>
-              <b-form-group
-                label="邀请码"
-                label-for="register_input5"
-                label-text-align="left"
-              >
-                <b-form-input
-                  id="register_input5"
-                  placeholder="请输入邀请码"
-                ></b-form-input>
-              </b-form-group> -->
-              <b-form-checkbox
-                id="checkbox1"
-                value="accepted"
-                unchceked-value="not_accepted"
-                class="float-left"
-              >
-                我已阅读并同意《用户协议》
-              </b-form-checkbox>
-              <b-button
-                variant="success"
-                size="lg"
-                class="mobile_login_lg mobile_register_lg"
-              >
-                注册
-              </b-button>
+                  <b-form-input
+                    id="register_input1"
+                    placeholder="请输入手机号"
+                    v-model="val.default"
+                    :name="val.name"
+                    v-if="val.input!=='hidden'&&val.input=='text'"
+                  ></b-form-input>
+                  <b-form-select
+                    :options="val.select"
+                    v-if="val.input!=='hidden'&&val.input=='select'"
+                    v-model="val.default"
+                    :name="val.name"
+                  >
+                  </b-form-select>
+                </b-form-group>
+                <b-form-checkbox
+                  id="checkbox1"
+                  value="accepted"
+                  unchceked-value="not_accepted"
+                  class="float-left"
+                >
+                  我已阅读并同意《用户协议》
+                </b-form-checkbox>
+                <b-button
+                  variant="success"
+                  size="lg"
+                  class="mobile_login_lg mobile_register_lg"
+                  type="submit"
+                >
+                  注册
+                </b-button>
+              </b-form>
               <b-button
                 variant="link"
                 size="lg"
@@ -125,21 +88,22 @@
 import bButton from 'bootstrap-vue/es/components/button/button';
 import bFormGroup from 'bootstrap-vue/es/components/form-group/form-group';
 import bFromCheckboxGroup from 'bootstrap-vue/es/components/form-checkbox/form-checkbox-group';
+import bForm from 'bootstrap-vue/es/components/form/form';
 import PanelLeft from '../PanelLeft';
 export default {
   data() {
     return {
-      reg_content:''
+      reg_content: ''
     }
   },
   created() {
-    this.$http.post(this.HOST+'api/webmember/register',{
-      type:1
-    }).then((res)=>{
+    this.$http.post(this.HOST + 'api/webmember/register', {
+      type: 1
+    }).then((res) => {
       console.log(JSON.stringify(res));
       this.reg_content = res.data.data.regdatasets;
       // console.log(this.reg_content);
-    }).catch((err)=>{
+    }).catch((err) => {
       console.log(JSON.stringify(err));
     })
   },
@@ -147,10 +111,18 @@ export default {
     [bButton.name]: bButton,
     [bFormGroup.name]: bFormGroup,
     [bFromCheckboxGroup.name]: bFromCheckboxGroup,
+    [bForm.name]: bForm,
     PanelLeft
   },
   methods: {
-    
+    onSubmit(evt){
+      evt.preventDefault();
+      var formData = document.getElementById('#form');
+      var formdata = new FormData(formData);
+      this.$http.post(this.HOST+'api/webmember/registersave',{
+        
+      })
+    }
   }
 }
 </script>
