ba6538d971b3464db755cb2209a46e06b7c0c091
chengjiang <15712859926@163.com>
2018-12-01 15:49:42 +0800

    奖金面板

--- src/pages/Prize.vue
+++ src/pages/Prize.vue
@@ -22,7 +22,10 @@
                   xl="3"
                   lg="3"
                 >
-                  <b-button :variant="ActiveId==1?'warning':'outline-success'">销售奖金表</b-button>
+                  <b-button
+                    :variant="ActiveId==1?'warning':'outline-success'"
+                    @click="routeChange(1)"
+                  >销售奖金表</b-button>
                 </b-col>
                 <b-col
                   sm="3"
@@ -31,7 +34,10 @@
                   xl="3"
                   lg="3"
                 >
-                  <b-button variant="warning">汇款通知</b-button>
+                  <b-button
+                    :variant="ActiveId==2?'warning':'outline-success'"
+                    @click="routeChange(2)"
+                  >汇款通知</b-button>
                 </b-col>
                 <b-col
                   sm="3"
@@ -40,7 +46,10 @@
                   xl="3"
                   lg="3"
                 >
-                  <b-button variant="warning">添加汇款通知</b-button>
+                  <b-button
+                    :variant="ActiveId==3?'warning':'outline-success'"
+                    @click="routeChange(3)"
+                  >添加汇款通知</b-button>
                 </b-col>
                 <b-col
                   sm="3"
@@ -49,9 +58,13 @@
                   xl="3"
                   lg="3"
                 >
-                  <b-button variant="warning">旅游奖信息</b-button>
+                  <b-button
+                    :variant="ActiveId==4?'warning':'outline-success'"
+                    @click="routeChange(4)"
+                  >旅游奖信息</b-button>
                 </b-col>
               </b-row>
+              <b-table></b-table>
             </b-col>
           </b-row>
         </b-container>
@@ -67,6 +80,7 @@ import '../assets/sass/login.sass';
 import Header from '../components/Header';
 import Footer1 from '../components/Footer1';
 import bButton from 'bootstrap-vue/es/components/button/button';
+import bTable from 'bootstrap-vue/es/components/table/table';
 export default {
   name: '',
   data() {
@@ -77,8 +91,10 @@ export default {
   created() {
     console.log(this.$route.params.id);
   },
-  updated() {
-    console.log(this.$route.params.id);
+  beforeRouteUpdate(to, from, next) {
+    console.log(to.params.id);
+    this.ActiveId = to.params.id;
+    next();
   },
   filters: {
 
@@ -86,7 +102,13 @@ export default {
   components: {
     Header,
     Footer1,
-    [bButton.name]: bButton
+    [bButton.name]: bButton,
+    [bTable.name]:bTable
+  },
+  methods: {
+    routeChange(index){
+      this.$router.replace(`/prize/${index}`);
+    }
   }
 }
 </script>
@@ -96,6 +118,9 @@ export default {
   background-color: #ed8134;
   border-color: #ed8134;
   color: #fff;
+  border-radius: 15px;
+}
+.btn-outline-success {
   border-radius: 15px;
 }
 </style>
